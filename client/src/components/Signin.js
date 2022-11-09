262de29fdbb7d0c27a8e87aef37e53586e94b048
star-trek703 <81070137+star-trek703@users.noreply.github.com>
2022-11-09 11:27:43 +0530

    Update Signin.js

--- client/src/components/Signin.js
+++ client/src/components/Signin.js
@@ -2,11 +2,12 @@ import { useState, useEffect } from "react"
 import axios from "axios"
 import { useNavigate } from "react-router-dom"
 
-const Signin = ({ isLoggedIn, setIsLoggedIn }) => {
+const Signin = ({ isLoggedIn, setIsLoggedIn, setUser }) => {
   const [formdata, setFormdata] = useState({
     email: '',
     password: ''
   })
+  const [error, setError] = useState('')
 
   const navigate = useNavigate()
 
@@ -28,15 +29,23 @@ const Signin = ({ isLoggedIn, setIsLoggedIn }) => {
   const onSubmit = async (e) => {
     e.preventDefault()
 
-    const res = await axios.post("/auth/signin", {
-      email, password
-    })
-    
-    if (res.status === 200) {
-      localStorage.setItem('user', JSON.stringify(res.data))
+    try {
+      const credentials = {
+        email, password
+      }
       
-      setIsLoggedIn(true)
-      navigate('/')
+      const res = await axios.post("/auth/signin", credentials)
+      
+      if (res.status === 200) {
+        localStorage.setItem('user', JSON.stringify(res.data))
+        
+        setUser(res.data)
+        setIsLoggedIn(true)
+        navigate('/')
+      }
+    } catch (error) {
+      const msg = error.response.data.message || error.message || 'Something went wrong'
+      setError(msg)
     }
   }
 
@@ -48,14 +57,18 @@ const Signin = ({ isLoggedIn, setIsLoggedIn }) => {
           <p className="text-gray-700 text-lg text-center">Login to your account</p>
         </div>
 
+        <div className="bg-red-700 text-white text-center my-3 py-1 rounded" style={{ display: error ? 'block' : 'none' }}>
+          { error }
+        </div>
+
         <form className="mt-3" onSubmit={ onSubmit }>
           <div className="py-1">
             <label className="block pb-1" htmlFor="email">Email</label>
-            <input className="block w-full px-3 py-1.5 border border-gray-300 focus:border-gray-500 rounded outline-none transition-all" type="text" name="email" id="email" value={ email } onChange={ onChange } />
+            <input className="block w-full px-3 py-1.5 border border-gray-300 focus:border-gray-500 rounded outline-none transition-all" type="text" name="email" id="email" placeholder="Email" value={ email } onChange={ onChange } />
           </div>
           <div className="py-1 mt-2">
             <label className="block pb-1" htmlFor="password">Password</label>
-            <input className="block w-full px-3 py-1.5 border border-gray-300 focus:border-gray-500 rounded outline-none transition-all" type="password" name="password" id="password" value={ password } onChange={ onChange } />
+            <input className="block w-full px-3 py-1.5 border border-gray-300 focus:border-gray-500 rounded outline-none transition-all" type="password" name="password" id="password" placeholder="Password" value={ password } onChange={ onChange } />
           </div>
           <div className="py-1 mt-5">
             <button className="block w-full bg-gray-900 hover:opacity-90 text-white px-3 py-2 border border-gray-300 focus:border-gray-500 rounded-md outline-none transition-all" type="submit">Login</button>
